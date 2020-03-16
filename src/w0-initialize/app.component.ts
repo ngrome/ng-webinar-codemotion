bb5503a5dada6b7a2de8e9a9d1908b765f3655ff
Raul Valentini <rvt@MacBook-Pro-di-Raul.local>
2020-03-16 22:24:11 +0100

    Initialize project

---
+++ src/w0-initialize/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-webinar-codemotion - w0-initialize';
+}
