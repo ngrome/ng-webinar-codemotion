5eec13234d0064e54a14c16326b0daa92b0dc5ff
Ermanno <ermanno.battista@gmail.com>
2020-09-26 17:11:09 +0200

    Add models for user activity and profile

---
+++ projects/w3-github-app/src/app/models/user-activity.ts
@@ -0,0 +1,4 @@
+export interface UserActivity {
+  entry: string;
+  createdAt: string;
+}
---
+++ projects/w3-github-app/src/app/models/user-profile.ts
@@ -0,0 +1,5 @@
+export interface UserProfile {
+  gitLogin?: string;
+  name: string;
+  surname: string;
+}
